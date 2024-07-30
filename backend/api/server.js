fec5d6c08ca1606d1b427dd4f2c6a7fc32e5497b
aryan262 <aryan.nikolas@gmail.com>
2024-07-31 00:06:34 +0530

    Part 1

--- backend/api/server.js
+++ backend/api/server.js
@@ -21,10 +21,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit:"20mb"}))
 app.use(express.urlencoded({extended:true}))
-app.use(cors({
-    origin:"*",
-    credentials: true
-}));
+app.use(cors());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
