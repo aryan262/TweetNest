96f249fdb0c8a553fd6fa50a8e1e623373560509
aryan262 <aryan.nikolas@gmail.com>
2024-07-31 00:12:30 +0530

    last push

--- backend/api/server.js
+++ backend/api/server.js
@@ -21,7 +21,11 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit:"20mb"}))
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://social-ackend-imvo.vercel.app/"
+    }
+));
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -31,6 +35,12 @@ app.get("/api/", (req, res)=>{
     res.send("Hello from server");
 }
 )
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
