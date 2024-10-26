283c6a5ecfd1c415185e9151863d3d89700b028f
Aryan Panchal <aryan.nikolas@gmail.com>
2024-10-26 18:46:42 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors(
     {
         origin: process.env.FRONTEND_URL,
-        credentials: true,
+        // credentials: true,
     }
 ));
 app.use(cookieParser())
