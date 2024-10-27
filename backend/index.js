443e953c78fffeca8cc4637e696faf651b47bd61
Aryan Panchal <aryan.nikolas@gmail.com>
2024-10-27 11:06:13 +0530

    Added methods

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ app.use(cors(
     {
         origin: ['https://tweetnest.vercel.app', 'http://localhost:5173'],
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"]
     }
 ));
 app.use(cookieParser())
